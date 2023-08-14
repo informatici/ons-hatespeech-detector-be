2e052330ff91251e1c95f88f1fa36aece42f530c
Carlo Bono <carloalberto.bono@polimi.it>
2023-08-14 20:11:32 +0200

    TAG: dev-be-202308141811

--- embedding_classifier.py
+++ embedding_classifier.py
@@ -33,5 +33,4 @@ class EmbeddingClassifier:
         return self.laser.embed_sentences(texts, lang='it')
 
     def classify(self, texts):
-        deleteme = self.embed(texts)
-        return True # self.clf.predict(self.embed(texts))
+        return self.clf.predict(self.embed(texts))
