a905382f51696a93a3dc830c45fddc8a4d79191c
mwithi <alessandro.domanico@yahoo.it>
2024-03-14 23:15:56 +0100

    Fix for security

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TAG=dev-be-`TZ=UTC date +%Y%m%d%H%M`
-git add .
-git commit -m "TAG: $TAG"
-git push
+#git add .
+#git commit -m "TAG: $TAG"
+#git push
 git tag $TAG
 git push origin $TAG
