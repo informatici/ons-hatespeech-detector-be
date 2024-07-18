fbc31da761bc3e41e4661b52b9039d6c053838c3
Carlo Bono <carloalberto.bono@polimi.it>
2024-07-18 08:57:39 +0200

    revert temporaneo modelli

--- unsupervised_models.py
+++ unsupervised_models.py
@@ -2,7 +2,7 @@
 import re
 import pickle
 
-VERSION = 13
+VERSION = 12
 
 class Hurtlext:
 
@@ -103,7 +103,8 @@ class HateSpeechDictionaryV2:
         self.regxs = d.groupby('group')['word'].apply(lambda x : '(\\b|^)(' + '|'.join(x).replace('*', '.*?\\b').replace('_', ' ') + ')(\\b|$)').to_dict()
 
         # Laser
-        self.emb = EmbeddingClassifierRKNN('model/clf.22062024.binary.pickle')
+        # self.emb = EmbeddingClassifierRKNN('model/clf.22062024.binary.pickle')
+        self.emb = EmbeddingClassifierRKNN('model/clf.20012024.binary.pkl')
 
     def score(self, p):
 
@@ -216,7 +217,8 @@ class AnswerChatterV2:
 
     def __init__(self):
         # Laser
-        self.emb = EmbeddingClassifierKNN('model/clf.22062024.multipass.pickle')
+        # self.emb = EmbeddingClassifierKNN('model/clf.22062024.multipass.pickle')
+        self.emb = EmbeddingClassifierKNN('model/clf.20012024.multipass.pickle')
         with open('model/mapping.risposte.pkl', 'rb') as f:
             self.mapping = pickle.load(f)
 
